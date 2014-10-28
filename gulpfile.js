feca3b49f07c947fec5512f0f796c5d620fe3a00
Yves Richard <yves.richard@tesera.com>
2014-10-28 06:58:05 -0600

    add example:copy task for example packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-var gulp = require('gulp'),
-    karma = require('karma').server,
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    sourceFiles = [
-        'src/acComponents/acComponents.prefix',
-        'src/acComponents/acComponents.js',
-        'src/acComponents/directives/**/*.js',
-        'src/acComponents/filters/**/*.js',
-        'src/acComponents/services/**/*.js',
-        'src/acComponents/templates/**/*.js',
-        'src/acComponents/acComponents.suffix'
-    ],
-    webserver = require('gulp-webserver'),
-    templateCache = require('gulp-angular-templatecache'),
-    jade = require('gulp-jade');
+var gulp = require('gulp');
+var karma = require('karma').server;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var ngAnnotate = require('gulp-ng-annotate');
+var webserver = require('gulp-webserver');
+var templateCache = require('gulp-angular-templatecache');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var sourceFiles = [
+    'src/acComponents/acComponents.prefix',
+    'src/acComponents/acComponents.js',
+    'src/acComponents/directives/**/*.js',
+    'src/acComponents/filters/**/*.js',
+    'src/acComponents/services/**/*.js',
+    'src/acComponents/templates/**/*.js',
+    'src/acComponents/acComponents.suffix'
+];
 
 gulp.task('example', function() {
     gulp.src('.')
@@ -43,6 +44,32 @@ gulp.task('build', function() {
         .pipe(uglify())
         .pipe(rename('ac-components.min.js'))
         .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('example:copy', function () {
+    var deps = {
+        css: [
+            './bower/mapbox.js/mapbox.css',
+            './bower/Leaflet.label/dist/leaflet.label.css',
+            './bower/leaflet.locatecontrol/src/L.Control.Locate.css'
+        ],
+        js: [
+            './bower/lodash/dist/lodash.min.js',
+            './bower/jquery/dist/jquery.min.js',
+            './bower/angular/angular.min.js',
+            './bower/angular-sanitize/angular-sanitize.min.js',
+            './bower/mapbox.js/mapbox.js',
+            './bower/Leaflet.label/dist/leaflet.label.js',
+            './bower/leaflet.locatecontrol/src/L.Control.Locate.js'
+        ]
+    };
+
+    for (var dep in deps) {
+        gulp.src(deps[dep])
+            .pipe(concat('vendor.'+dep))
+            .pipe(gulp.dest('./example/'+dep));
+    }
+
 });
 
 /**
