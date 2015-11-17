857e2d50211f71eea41b4a6da7c8fe4024e1fbf4
Bogdan Cochior <bogdan.cochior@thinslices.com>
2015-11-17 10:09:55 +0200

    Change All_min icon to SVG

---
+++ src/acComponents/directives/acAllMinIcon.js
@@ -0,0 +1,10 @@
+angular.module('acComponents.directives')
+  .directive('acAllminIcon', function () {
+    return {
+      replace: true,
+      templateUrl: 'allmin-icon.html',
+      link: function ($scope, el, attrs) {
+
+      }
+    };
+  });
