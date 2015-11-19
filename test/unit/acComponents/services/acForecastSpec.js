62e639600c996243808c4e38b547ee488bbbf2c0
Iulian Gioada <iulian.gioada@thinslices.com>
2015-11-19 20:07:58 +0200

    Add unit tests and fix drawer

--- test/unit/acComponents/services/acForecastSpec.js
+++ test/unit/acComponents/services/acForecastSpec.js
@@ -1,14 +1,15 @@
 'use strict';
 
 describe("acComponents.services.acForecast", function() {
-    var $httpBackend, acForecast;
+    var $httpBackend, acForecast, apiUrl;
 
     beforeEach(module('acComponents.config'));
     beforeEach(module('acComponents.services'));
 
-    beforeEach(inject(function(_$httpBackend_, _acForecast_) {
+    beforeEach(inject(function(_$httpBackend_, _acForecast_, _AC_API_ROOT_URL_) {
         $httpBackend = _$httpBackend_;
         acForecast = _acForecast_;
+        apiUrl = _AC_API_ROOT_URL_;
     }));
 
     afterEach(function () {
@@ -18,21 +19,21 @@ describe("acComponents.services.acForecast", function() {
 
     describe("fetch", function() {
 
-        it("requests forecasts bby relative path", function () {
-            $httpBackend.expectGET('/api/forecasts').respond(200, {});
+        it("requests forecasts by relative path", function () {
+            $httpBackend.expectGET(apiUrl+'/api/forecasts').respond(200, {});
             acForecast.fetch();
             $httpBackend.flush();
         });
 
         it("caches forecasts after first fetch", function () {
-            $httpBackend.expectGET('/api/forecasts').respond(200, {});
+            $httpBackend.expectGET(apiUrl+'/api/forecasts').respond(200, {});
             acForecast.fetch();
             $httpBackend.flush();
             acForecast.fetch();
         });
 
         it("returns a promise", function () {
-            $httpBackend.expectGET('/api/forecasts').respond(200, {});
+            $httpBackend.expectGET(apiUrl+'/api/forecasts').respond(200, {});
             var p = acForecast.fetch();
             $httpBackend.flush();
 
@@ -40,7 +41,7 @@ describe("acComponents.services.acForecast", function() {
         });
 
         it("returns a promise even if forcasts are cached", function () {
-            $httpBackend.expectGET('/api/forecasts').respond(200, {});
+            $httpBackend.expectGET(apiUrl+'/api/forecasts').respond(200, {});
             acForecast.fetch();
             $httpBackend.flush();
 
---
+++ test/unit/acComponents/services/acFormUtilsSpec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe("acComponents.services.acFormUtils", function() {
+  var acFormUtils, mockFields;
+
+  beforeEach(module('acComponents.services'));
+
+  beforeEach(inject(function(_acFormUtils_) {
+    acFormUtils = _acFormUtils_;
+    mockFields = {
+      number: {
+        type: 'number',
+        value: null
+      },
+      checkbox: {
+        type: 'checkbox',
+        options: {
+          opt1: false,
+          opt2: false
+        }
+      },
+      radio: {
+        type: 'radio',
+        options: ['opt1', 'opt2', 'opt3'],
+        value: null
+      },
+      dropdown: {
+        type: 'dropdown',
+        options: ['opt1', 'opt2', 'opt3'],
+        value: null
+      }
+    };
+  }));
+
+  describe("buildReport", function() {
+
+    it("should return an object", function () {
+      var report = acFormUtils.buildReport(mockFields);
+
+      expect(angular.isObject(report)).to.equal(true);
+    });
+
+
+  });
+});
