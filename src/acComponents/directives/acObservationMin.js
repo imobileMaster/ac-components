10e5e06954fa14426088a3270b2a15f49fde8b91
Bogdan Cochior <bogdan.cochior@thinslices.com>
2015-11-25 14:32:24 +0200

    Add Report full page directive Also, some refactoring on quick report display type

--- src/acComponents/directives/acObservationMin.js
+++ src/acComponents/directives/acObservationMin.js
@@ -8,7 +8,6 @@ angular.module('acComponents.directives')
       },
       link: function (scope, el, attrs) {
         el.addClass('ac-observation-drawer');
-        el.addClass(attrs.acObservationMin);
 
         scope.activeTab = {};
         scope.reportTypes = acConfig.reportTypes;
@@ -61,14 +60,6 @@ angular.module('acComponents.directives')
             return newObj.requested === ob.obtype;
           });
 
-          if (newObj.requested === 'quick') {
-            requestedObj = _.filter(newObj.obs, function (ob) {
-              return newObj.requested === ob.obtype;
-            });
-            scope.activeTab = mapQuickObject(requestedObj[0].ob);
-            return;
-          }
-
           if (requestedObj[0].ob) {
             scope.activeTab = acReportData[newObj.requested].mapDisplayResponse(requestedObj[0].ob);
           }
@@ -89,96 +80,10 @@ angular.module('acComponents.directives')
           }
         }
 
-        function mapQuickObject(ob) {
-          var quickTab = [];
-
-          if (ob.avalancheConditions && mapAvalancheConditions(ob.avalancheConditions).length > 0) {
-            quickTab.push({
-              prompt: 'Avalanche conditions',
-              type: 'checkbox',
-              order: 2,
-              value: mapAvalancheConditions(ob.avalancheConditions)
-            });
-          }
-
-          _.forEach(ob.ridingConditions, function (item, key) {
-            if (item.type === 'single' && item.selected) {
-              if (item.selected) {
-                quickTab.push({
-                  prompt: item.prompt,
-                  type: 'radio',
-                  order: 1,
-                  value: item.selected
-                });
-              }
-            }
-
-            if (item.type === 'multiple' && item.options) {
-              var selected = _.reduce(item.options, function (select, it, key) {
-                if (it) {
-                  select.push(key);
-                }
-                return select;
-              },[]);
-
-              if (!_.isEmpty(selected)){
-                quickTab.push({
-                  prompt: item.prompt,
-                  type: 'checkbox',
-                  order: 1,
-                  value: selected
-                });
-              }
-            }
-          });
-
-          return quickTab;
-        }
-
-        function mapAvalancheConditions(av) {
-          var avalanches = [];
-          if (av.slab) {
-            avalanches.push('Slab avalanches today or yesterday.');
-          }
-          if (av.sound) {
-            avalanches.push('Whumphing or drum-like sounds or shooting cracks.');
-          }
-          if (av.snow) {
-            avalanches.push('30cm + of new snow, or significant drifitng, or rain in the last 48 hours.');
-          }
-          if (av.temp) {
-            avalanches.push('Rapid temperature rise to near zero degrees or wet surface snow.');
-          }
-          return avalanches;
-        }
-
         function viewFullPage(id){
           $state.go('ac.reports', { subid:id });
         }
 
       }
     };
-  })
-  .filter('dateformat', function(){
-    return function formatDate(datetimeString){
-      var datetime = moment(datetimeString);
-      var offset = moment.parseZone(datetimeString).zone();
-      var prefixes = {
-        480: 'P',
-        420: 'M',
-        360: 'C',
-        300: 'E',
-        240: 'A',
-        180: 'N'
-      };
-      var suffix = datetime.isDST() ? 'DT' : 'ST';
-      var zoneAbbr = 'UTC';
-
-      if(offset in prefixes) {
-        zoneAbbr = prefixes[offset] + suffix;
-        datetime.subtract(offset, 'minutes');
-      }
-
-      return datetime.format('MMM Do, YYYY [at] HH:mm [' + zoneAbbr + ']')
-    }
   });
