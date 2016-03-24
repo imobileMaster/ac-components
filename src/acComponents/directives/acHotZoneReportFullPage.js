14488beb56287b45e5927805fe81e65a175fee7f
Mandy Ferrari <mandy.ferrari@tesera.com>
2016-03-24 09:55:58 -0600

    Add backend and template for hzr full page report

---
+++ src/acComponents/directives/acHotZoneReportFullPage.js
@@ -0,0 +1,38 @@
+angular.module('acComponents.directives')
+  .directive('acHotZoneReportFullPage', function (acReportData) {
+    return {
+      templateUrl: 'hot-zone-report-fullpage.html',
+      scope: {
+        sub: '=report',
+        sponsor: '=sponsor'
+      },
+      link: function (scope, el, attrs) {
+        el.addClass('ac-observation-drawer');
+
+        scope.print = print;
+        scope.activeReports = [];
+
+        processReportInfo();
+
+        function processReportInfo() {
+          scope.activeReports = _.reduce(scope.sub.obs, function (results, item, key){
+                results.push({
+                  obtype: item.obtype,
+                  ob: acReportData[item.obtype].mapDisplayResponse(item.ob)
+                });
+              return results;
+          },[]);
+
+          scope.activeReports = _.sortBy(scope.activeReports, function(obj) {
+            return ['quick', 'avalanche', 'snowpack', 'weather', 'incident'].indexOf(obj.obtype);
+          });
+
+        }
+
+        function print(){
+          window.print();
+        }
+
+      }
+    };
+  });
