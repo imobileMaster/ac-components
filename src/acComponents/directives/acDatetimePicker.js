cf4e5261629abff18564bfbdc0e139c7e42728cd
Mandy Ferrari <mandy.ferrari@tesera.com>
2016-04-18 17:23:17 -0600

    Adjust min date in hzr report form

--- src/acComponents/directives/acDatetimePicker.js
+++ src/acComponents/directives/acDatetimePicker.js
@@ -4,21 +4,23 @@ angular.module('acComponents.directives')
           scope: {
             showOnlyDate: '=',
             showOnlyTime: '=',
-            maxDateToday: '='
+            maxDateToday: '=',
+            minDateToday: '='
           },
           link: function (scope, el) {
             if(jQuery().datetimepicker) {
-              
+
 
               var options = {
                 maxDate: (scope.maxDateToday === true) ? new Date() : new Date(new Date().setYear(new Date().getFullYear() + 1)),
+                minDate: (scope.minDateToday === true) ? new Date() : new Date(new Date().setYear(new Date().getFullYear() + 1)),
                 pickTime: !scope.showOnlyDate,
                 format: (scope.showOnlyTime ? 'LT' : scope.showOnlyDate ? "YYYY-MM-DD" : "YYYY-MM-DD hh:mm A"),
                 pickDate: scope.showOnlyTime ? !scope.showOnlyTime : true
               };
 
               el.datetimepicker(options);
-              
+
               jQuery(window).scroll(function() {
                 if(el.data("DateTimePicker") !== undefined) {
                     el.data("DateTimePicker").hide();
